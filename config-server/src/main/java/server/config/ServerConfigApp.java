094ad4d69f85a70d070c1c51293982e1175be4db
ruslan <leshchinskyruslan@gmail.com>
2025-04-13 19:16:36 +0300

    do first local config for book-service

--- config-server/src/main/java/server/config/ServerConfigApp.java
+++ config-server/src/main/java/server/config/ServerConfigApp.java
@@ -1,7 +1,11 @@
+package server.config;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@SpringBootApplication
 @EnableConfigServer
 @EnableEurekaClient
-public class ConfigServer {
+public class ServerConfigApp {
 }
