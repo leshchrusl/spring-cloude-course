1e1bd1a23a60b2e46ceb0738cf3469cf2dbe4bef
ruslan <leshchinskyruslan@gmail.com>
2025-07-03 15:48:49 +0300

    done circuit breaker first version

--- client-service/src/main/java/com/example/clientservice/feignclient/BookFeign.java
+++ client-service/src/main/java/com/example/clientservice/feignclient/BookFeign.java
@@ -1,12 +1,13 @@
 package com.example.clientservice.feignclient;
 
 import com.example.clientservice.entity.Book;
+import com.example.clientservice.fallback.Fallback;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
 
-@FeignClient(name = "book-service")
+@FeignClient(name = "book-service", fallback = Fallback.class)
 public interface BookFeign {
 
     @GetMapping("/api/book/all")
